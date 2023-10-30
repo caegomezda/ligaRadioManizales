f47670d75ca0451239cd75fa5597d6e626e59ec6
Edugoda314 <camiloedg23@gmail.com>
2023-10-30 12:19:49 -0700

    Menu changes

--- src/app/componente/menu/menu.component.ts
+++ src/app/componente/menu/menu.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
-
+  constructor(private route: ActivatedRoute) {}
+  isRouteActive(routePath: string) {
+    if (this.route.snapshot.routeConfig) {
+      return this.route.snapshot.routeConfig.path === routePath;
+    }else{
+      return null
+    }
+  }
 }
